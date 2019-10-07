ba9da6ce32b35db9935f0147a126c5aee857513b
jusselman <usselmanjosh@gmail.com>
2019-10-06 17:21:35 -0700

    login register helper

--- controllers/user.js
+++ controllers/user.js
@@ -1,2 +1,12 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.SECRET;
+
+function createJWT(user) {
+    return jwt.sign(
+        { user },
+        SECRET,
+        { expiresIn: '24h' }
+    );
+}
