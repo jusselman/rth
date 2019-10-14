f567b7c61601167dc2e393678d428724751bba34
jusselman <usselmanjosh@gmail.com>
2019-10-14 08:47:53 -0700

    heroku build

--- src/components/BeatPad/BeatPad.jsx
+++ src/components/BeatPad/BeatPad.jsx
@@ -4,6 +4,8 @@ import './BeatPad.css';
 const BeatPad = (props) => (
     <div>
         <h1>Beats on the Fly</h1>
+        <img src="https://imgur.com/9c319f46-7dc6-4104-9dae-16871cf6fbc1"></img>
+
 
     </div>
 )
