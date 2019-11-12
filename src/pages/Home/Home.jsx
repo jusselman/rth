439ef11337cd648a8c2b3bee3295dced43614900
jusselman <usselmanjosh@gmail.com>
2019-11-11 18:05:45 -0800

    anim on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css';
 
 const Home = (props) => {
     return (
-        <div className="Home">
+        <div className="Home anim">
             <h1>RTHH</h1>
             <h2>Make Beats on the fly</h2>
             <h2>Upload, Share, Link</h2>
