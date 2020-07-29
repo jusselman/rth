afc3cc17d7cd01effaef6ebb9014fd1628e5ceb3
jusselman <usselmanjosh@gmail.com>
2020-07-28 19:48:17 -0700

    troubleshooting

--- src/pages/BeatPadPage/BeatPadPage.jsx
+++ src/pages/BeatPadPage/BeatPadPage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import BeatMachine from '../../components/BeatMachine/BeatMachine';
+import PlayButton from '../../components/Buttons/PlayButton';
+import RecordButton from '../../components/Buttons/RecordButton';
+import StopButton from '../../components/Buttons/StopButton';
+import SaveButton from '../../components/Buttons/SaveButton';
 import ControlRow from '../../components/ControlRow/ControlRow';
 import './BeatPadPage.css';
 
@@ -9,7 +13,12 @@ const BeatPadPage = () => {
             <div className="beat-container">
                 <div className="pads">
                     <BeatMachine />
-                    <ControlRow />
+                    <div className="control-row-container">
+                        <RecordButton />
+                        <PlayButton />
+                        <StopButton />
+                        <SaveButton />
+                    </div>
                 </div>
             </div>
         </div>
