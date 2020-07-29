783c8ba0e0307303e17bbfdc062da5f849d2f451
jusselman <usselmanjosh@gmail.com>
2020-07-28 18:46:50 -0700

    trouble shooting

--- src/pages/BeatPadPage/BeatPadPage.jsx
+++ src/pages/BeatPadPage/BeatPadPage.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import BeatMachine from '../../components/BeatMachine/BeatMachine';
-import './BeatPadPage.css';
 import ControlRow from '../../components/ControlRow/ControlRow';
 import AudioRecorder from '../../components/AudioRecorder/AudioRecorder';
+import './BeatPadPage.css';
 
 const BeatPadPage = () => {
     return (
         <div className="BeatPadPage">
             <div className="beat-container">
                 <div className="pads">
-                    <ControlRow />
+
                 </div>
-                {/* <img src="https://images.unsplash.com/photo-1535979014625-490762ceb2ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" /> */}
+
             </div>
         </div>
     )
